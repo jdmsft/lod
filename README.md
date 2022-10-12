a208a5f85a2bb3316805414ea0ed58fd820778c2
jdmsft <25929992+jdmsft@users.noreply.github.com>
2022-10-12 09:43:49 +0200

    INIT

---
+++ README.md
@@ -0,0 +1,61 @@
+# Microsoft LOD
+
+Microsoft Lab On Demand (LOD) cheatsheet.
+
+## 1. Access to LOD
+
+Follow trainer presentation to redeem a training key to access to your virtual classroom and your virtual machine in the LOD platform.
+
+> Normally you've already done this part! Let's jump to the [second part](#2-update-lab-environment) 😉
+
+## 2. Update lab environment
+
+To update your LOD virtual machine before performing the lab follow the process below:
+
+1. Connect to your LOD virtual machine
+
+![](./media/type-password.png)
+
+2. In your LOD virtual machine, open a PowerShell console as administrator
+
+![](./media/powershell-as-admin.png)
+
+3. Click on `⚡` icon at the upper-left of your lab window and select `Type Text` > `Type Clipboard Text` 
+
+![](./media/type-clipboard-text.png)
+
+4. Paste the code below in the `Type Clipboard Text` window and click `OK`
+
+Code to paste:
+
+````powershell
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+iex ((New-Object System.Net.WebClient).DownloadString('https://jdmsft.com/lod-script'))
+````
+
+In this window:
+
+![](./media/type-clipboard-text-2.png)
+
+Don't forget to click on `OK` button!
+
+5. Press `ENTER` key in your VM PowerShell console to run the code.
+
+![](./media/powershell-1.png)
+
+6. Press `ENTER` again to finalize the lab update.
+
+![](./media/powershell-2.png)
+
+**Update done!** (the PowerShell console will automatically close by itself at the end of the process)
+
+## 3. Claim Azure Pass
+
+Go back to trainer presentation to claim a new Azure Pass subscription together.
+
+## 4. Connect to Azure
+
+Open a new PowerShell console and use `Connect-AzAccount` cmdlet with your credential (used during Azure Pass phase) to connect to your Azure Pass subscription!
+
+Enjoy your lab! 😊 (the lab content is directly available from the right pane of your LOD window)
+
---
+++ script/Initialize-LOD.ps1
@@ -0,0 +1,59 @@
+﻿<#
+    .NOTES
+        ==============================================================================================
+        Copyright (c) 2022 JDMSFT. All rights reserved.
+        ==============================================================================================
+        Script:         Initialize-LOD
+        Purpose:        Initialize Microsoft LOD virtual machine Azure PowerShell environment
+        Version:        1.2
+        DateCreated:    11/10/22
+        DateModified:   11/10/22
+        ==============================================================================================
+        DISCLAIMER
+        ==============================================================================================
+        This script is not supported under any Microsoft standard support program or service.
+        This script is provided AS IS without warranty of any kind.
+        Microsoft further disclaims all implied warranties including, without limitation, 
+        any implied warranties of merchantability or of fitness for a particular purpose.
+        The entire risk arising out of the use or performance of the script
+        and documentation remains with you. In no event shall Microsoft, its authors,
+        or anyone else involved in the creation, production, or delivery of the
+        script be liable for any damages whatsoever (including, without limitation,
+        damages for loss of business profits, business interruption, loss of business
+        information, or other pecuniary loss) arising out of the use of or inability
+        to use the sample scripts or documentation, even if Microsoft has been
+        advised of the possibility of such damages.
+        ==============================================================================================
+        HOW TO USE
+        ==============================================================================================
+        Execute this script directly from VM by running this PowerShell (as Administrator) next line :
+        iex ((New-Object System.Net.WebClient).DownloadString('https://jdmsft.com/lod'))
+        ==============================================================================================
+        ==============================================================================================
+    .SYNOPSIS
+        Initialize Microsoft LOD virtual machine Azure PowerShell environment
+    .DESCRIPTION
+        Initialize Microsoft LOD virtual machine Azure PowerShell environment
+    .EXAMPLE
+        C:\PS> .\Initialize-LOD
+#>
+
+cls
+Write-Host "`nMicrosoft LOD Initializer v1.2 (JDMSFT - 2022)`n" -ForegroundColor Yellow
+Write-Warning "This script is intented to configure Microsoft LOD virtual machine ONLY!"
+Read-Host -Prompt "`nPress ENTER to continue or close this window to abort."
+
+Write-Host "Switching to AZERTY keyboard..."
+Set-WinUserLanguageList -LanguageList fr-fr -Force
+
+Write-Host "Enforcing TLS 1.2..."
+New-Item "C:\Users\StudentPC\Documents\WindowsPowerShell\Microsoft.PowerShell_profile.ps1" -Value '[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12' -Force | Out-Null
+
+Write-Host "Trusting PSGallery..."
+Set-PSRepository -Name 'PSGallery' -InstallationPolicy Trusted
+
+Write-Host "Updating Az modules..."
+Update-Module Az
+
+Write-Host "Closing PowerShell console"
+exit
