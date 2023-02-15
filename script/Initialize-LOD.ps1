6da89e14acba4f354e8a0937be01b2b649de984c
jdmsft <25929992+jdmsft@users.noreply.github.com>
2023-02-15 17:18:25 +0100

    Add additional lab optmizations

--- script/Initialize-LOD.ps1
+++ script/Initialize-LOD.ps1
@@ -1,13 +1,13 @@
 ﻿<#
     .NOTES
         ==============================================================================================
-        Copyright (c) 2022 JDMSFT. All rights reserved.
+        Copyright (c) 2022-2023 JDMSFT. All rights reserved.
         ==============================================================================================
         Script:         Initialize-LOD
         Purpose:        Initialize Microsoft LOD virtual machine Azure PowerShell environment
-        Version:        1.3
+        Version:        1.4
         DateCreated:    11/10/22
-        DateModified:   12/10/22
+        DateModified:   15/02/23
         ==============================================================================================
         DISCLAIMER
         ==============================================================================================
@@ -33,7 +33,7 @@
 #>
 
 cls
-Write-Host "`nMicrosoft LOD Initializer v1.3 (JDMSFT - 2022)`n" -ForegroundColor Yellow
+Write-Host "`nMicrosoft LOD Initializer v1.4 (JDMSFT - 2023)`n" -ForegroundColor Yellow
 Write-Warning "This script is intented to configure Microsoft LOD virtual machine ONLY!"
 Read-Host -Prompt "`nPress ENTER to continue or close this window to abort."
 
@@ -45,6 +45,34 @@ New-Item "C:\Users\StudentPC\Documents\WindowsPowerShell\Microsoft.PowerShell_pr
 
 Write-Host "Trusting PSGallery..."
 Set-PSRepository -Name 'PSGallery' -InstallationPolicy Trusted
+
+Write-Host 'Disabling Windows Update (Automatic Update)...'
+Stop-Service wuauserv
+Set-Service wuauserv -StartupType Disabled
+New-Item -Path "HKLM:SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate"
+New-Item -Path "HKLM:SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate\AU"
+Set-ItemProperty -Path "HKLM:SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate\AU" -Name NoAutoUpdate -Value 1
+
+Write-Host 'Disabling Microsoft Edge Update...'
+Stop-Service edgeupdate
+Set-Service edgeupdate -StartupType Disabled
+Stop-Service edgeupdatem
+Set-Service edgeupdatem -StartupType Disabled
+
+Write-Host "Disabling Acrobat Reader and Acrobat Manager Update..."
+Stop-Service AdobeARMservice
+Set-Service AdobeARMservice -StartupType Disabled
+
+Write-Host "Disabling Microsoft Compatibility Telemetry..."
+Get-ScheduledTask -TaskPath "\Microsoft\Windows\Application Experience\" | Disable-ScheduledTask
+
+# Download lab files locally (if applicable)
+# For WSPLUS Azure Automation
+If (Test-Path "C:\Users\Student\Desktop\Copy-StudentFiles.ps1") 
+{ 
+    Write-Host "Downloading student files (in another PS window)..."
+    Start-Process PowerShell -ArgumentList "-command C:\Users\Student\Desktop\Copy-StudentFiles.ps1" 
+}
 
 Write-Host "Updating Az modules..."
 Update-Module Az
